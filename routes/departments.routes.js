0f9c6c5cd6943bc1eccd73231b0b21d2a6343ac4
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-28 01:05:07 +0200

    Update db Methods findOneAndUpdate / Delete to have an overview of what has changed

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -47,14 +47,13 @@ router.put('/departments/:id', async (req, res) => {
   const { name } = req.body;
 
   try {
-    const dep = await Department.findById(req.params.id);
-    if (dep) {
-      await Department.updateOne(
-        { _id: req.params.id },
-        { $set: { name: name } }
-      );
-      res.json({ message: 'OK' });
-    } else res.status(404).json({ message: 'Not found...' });
+    const dep = await Department.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: { name: name } },
+      { new: true }
+    );
+    if (!dep) res.status(404).json({ message: 'Not found' });
+    else res.json(dep);
   } catch (err) {
     res.status(500).json({ message: err });
   }
@@ -62,11 +61,9 @@ router.put('/departments/:id', async (req, res) => {
 
 router.delete('/departments/:id', async (req, res) => {
   try {
-    const dep = await Department.findById(req.params.id);
-    if (dep) {
-      await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
-    } else res.status(404).json({ message: 'Not found...' });
+    const dep = await Department.findOneAndDelete({ _id: req.params.id });
+    if (!dep) res.status(404).json({ message: 'Not found' });
+    else res.json(dep);
   } catch (err) {
     res.status(500).json({ message: err });
   }
