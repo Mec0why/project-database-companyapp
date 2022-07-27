fb2216611f0746abc55ac23ede91118c83a30eb7
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-27 22:55:38 +0200

    Add schemas and models to Routes Employees and Products

--- models/employee.model.js
+++ models/employee.model.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const employeesSchema = new mongoose.Schema({
-  _id: { type: mongoose.Types.ObjectId, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   department: { type: String, required: true },
 });
+
+module.exports = mongoose.model('Employee', employeesSchema);
