3dea0032301f7bef5ac54ba140d0263182c41e4b
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-08-12 09:18:16 +0200

    Update CRUD Create test

--- models/test/department.crud.test.js
+++ models/test/department.crud.test.js
@@ -38,12 +38,13 @@ describe('Department', () => {
   });
   describe('Creating data', () => {
     it('should insert new document with "insertOne" method', async () => {
-        const department = new Department({ name: 'Department #1' });
-        await department.save();
-        const savedDepartment = await Department.findOne({ name: 'Department #1' });
-        expect(savedDepartment).to.not.be.null;
-      });      
+      const department = new Department({ name: 'Department #1' });
+      await department.save();
+      const savedDepartment = await Department.findOne({
+        name: 'Department #1',
+      });
       expect(savedDepartment).to.not.be.null;
     });
+    expect(savedDepartment).to.not.be.null;
   });
 });
