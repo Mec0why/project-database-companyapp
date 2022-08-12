851c1517ed91250334b307ea9ee539b26eba7ea0
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-08-12 14:31:21 +0200

    Add + import Plugin to simulate http requests

--- test/api/departments/get.test.js
+++ test/api/departments/get.test.js
@@ -1,4 +1,10 @@
-const expect = require('chai').expect;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+const request = chai.request;
 
 describe('GET /api/departments', () => {
 
