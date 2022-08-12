053e3c9492e1cd671d0c31f7d61b4de3104be9fd
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-08-12 15:09:46 +0200

    Prepare department/GET test db data

--- test/api/departments/get.test.js
+++ test/api/departments/get.test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../../server.js');
+const Department = require('../../models/department.model');
 
 chai.use(chaiHttp);
 
@@ -8,9 +9,26 @@ const expect = chai.expect;
 const request = chai.request;
 
 describe('GET /api/departments', () => {
+  before(async () => {
+    const testDepOne = new Department({
+      _id: '5d9f1140f10a81216cfd4408',
+      name: 'Department #1',
+    });
+    await testDepOne.save();
+
+    const testDepTwo = new Department({
+      _id: '5d9f1159f81ce8d1ef2bee48',
+      name: 'Department #2',
+    });
+    await testDepTwo.save();
+  });
+
   it('/ should return all departments', () => {});
 
   it('/:id should return one department by :id ', () => {});
 
   it('/random should return one random department', () => {});
+  after(async () => {
+    await Department.deleteMany();
+  });
 });
